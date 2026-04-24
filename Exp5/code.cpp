37b860dfe37b88cb5bdd5f7203c87c4ef46b54dc
Shresze <shreshta0611@gmail.com>
2026-04-24 23:10:38 +0530

    Update code.cpp

--- Exp5/code.cpp
+++ Exp5/code.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long l=1;
+        long long s=0;
+        for (long long b:batteries){
+            s+=b;
+        }
+        long long a=0;
+        long long r=s/n;
+        while (l<=r){
+            long long m=l+(r-l)/2;
+            if(check(n,batteries,m)){
+                a=m;
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+        return a;
+    }
 
+    bool check(int n,vector<int>& batteries,long long t) {
+        long long to=0;
+        for (long long b:batteries) {
+            to+=min(b,t);
+        }
+        
+        return to>=t*n;
+    }
+
+    
+};
