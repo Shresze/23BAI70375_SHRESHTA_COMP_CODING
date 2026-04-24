cf58ca8005e45d61ff0ca8e94bdc4ed7336e681e
Shresze <shreshta0611@gmail.com>
2026-04-24 23:07:39 +0530

    Update code.cpp

--- Exp4/code.cpp
+++ Exp4/code.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+#define mod 1000000007
+    int nthMagicalNumber(int n, int a, int b) {
 
+        long lcm=a*b/__gcd(a,b);
+        long l=min(a,b);
+        long h=n*l;
+        while(l<h){
+            long m=l+((h-l)/2);
+            long c=m/a+m/b-m/lcm;
+            if(c<n){
+                l=m+1;
+            }
+            else{
+                h=m;
+            }
+        }
+        return l%mod;
+    }
+};
